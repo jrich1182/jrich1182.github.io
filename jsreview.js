16ce02105dfa8edc383555bb12d943740f69f808
jrich1182 <jrich118@lab.cpcc.edu>
2025-07-10 16:53:32 -0400

    Update jsreview.js

--- jsreview.js
+++ jsreview.js
@@ -2,3 +2,25 @@ var course = "Technology";
 let myName = "Jasmine";
 
 console.log(`This page was created in the STEM PREP ${course} by ${myName}`);
+
+alert(`This page was created in the STEM PREP ${course} by ${myName}`);
+
+function output()
+{
+let name=document.getElementById("name").value;
+var school = document.getElementById("school").value;
+let grade= document.getElementById("grade").value;
+
+let output= "Name: <b>" + name + "</b><br>";
+output += "Grade: <b>" + grade + "</b><br>";
+output += "School: <b>" + school + </b><br>";
+
+  document.getElementById("output).innnerHTML = output;
+}
+
+
+
+
+
+
+
