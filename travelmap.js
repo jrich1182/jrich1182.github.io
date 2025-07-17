996a154ffb972ca572e6e25e66313ca16d765772
jrich1182 <jrich118@lab.cpcc.edu>
2025-07-17 16:05:25 -0400

    Create travelmap.js

---
+++ travelmap.js
@@ -0,0 +1,12 @@
+//40.7605° N, -73.9510° W
+var coordinates = [40.7605, -73.9510];
+
+let map = L.map("map", {
+  center: coordinates,
+  zoom: 10
+});
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
