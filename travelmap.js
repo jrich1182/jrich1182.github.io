71330adeb264b14da07e51ab58d959c5eaa8a9a9
jrich1182 <jrich118@lab.cpcc.edu>
2025-07-17 16:46:54 -0400

    Update travelmap.js

--- travelmap.js
+++ travelmap.js
@@ -10,3 +10,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+let circleMarker = L.circle(coordinates, {
+  color: "green",
+  fillColor: "yellow",
+  fillOpacity: 0.25,
+  radius: 50
+}).bindPopup(`<iframe width="280" height="160" src="https://www.youtube.com/embed/rzkt02T5LJI?si=35mg2FN0u6jM1TrU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`).addTo(map);
